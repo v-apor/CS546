2fb7f03b91fa010a0b02fe1500e391596dfb383a
v-apor <apoorv.learn@gmail.com>
2023-03-25 00:52:03 -0400

    Lab 6 | Updates

--- Class 7/lab/routes/albums.js
+++ Class 7/lab/routes/albums.js
@@ -1,8 +1,8 @@
 // Import the express router as shown in the lecture code
 // Note: please do not forget to export the router!
 
-import * as albums from "../data/albums.js";
-import * as bands from "../data/bands.js";
+import * as albumFunctions from "../data/albums.js";
+import * as bandFunctions from "../data/bands.js";
 
 import { Router } from "express";
 const router = Router();
@@ -10,31 +10,31 @@ const router = Router();
 router
     .route("/:bandId")
     .get(async (req, res) => {
-        const bandAlbums = await albums.getAll(req.params.bandId);
+        const bandAlbums = await albumFunctions.getAll(req.params.bandId);
         res.send(bandAlbums);
     })
     .post(async (req, res) => {
         const body = req.body;
         // console.log(body);
-        const newAlbum = await albums.create(
+        const newAlbum = await albumFunctions.create(
             req.params.bandId,
             body.title,
             body.releaseDate,
             body.tracks,
             body.rating
         );
-        const updatedBand = await bands.get(req.params.bandId);
+        const updatedBand = await bandFunctions.get(req.params.bandId);
         res.send(updatedBand);
     });
 
 router
     .route("/album/:albumId")
     .get(async (req, res) => {
-        const album = await albums.get(req.params.albumId);
+        const album = await albumFunctions.get(req.params.albumId);
         res.send(album);
     })
     .delete(async (req, res) => {
-        const album = await albums.remove(req.params.albumId);
+        const album = await albumFunctions.remove(req.params.albumId);
         res.send({ albumId: req.params.albumId, deleted: true });
     });
 
--- Class 7/lab/routes/bands.js
+++ Class 7/lab/routes/bands.js
@@ -17,6 +17,7 @@ router
     })
     .post(async (req, res) => {
         const body = req.body;
+        console.log(body);
         const newBand = await bands.create(
             body.name,
             body.genre,
