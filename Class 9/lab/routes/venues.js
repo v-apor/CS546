1ccf4123dc0db36c6925a3796fbb5e2d732f13c0
v-apor <apoorv.learn@gmail.com>
2023-04-06 02:10:23 -0400

    Update lab 8 to include some CSS and error pages

--- Class 9/lab/routes/venues.js
+++ Class 9/lab/routes/venues.js
@@ -10,23 +10,38 @@ router.route("/").get(async (req, res) => {
 });
 
 router.route("/searchvenues").post(async (req, res) => {
-    const url = `https://app.ticketmaster.com/discovery/v2/venues.json?keyword=${req.body.query}&apikey=JsrX5LuYMMOMbdo51ofnq8ud8gHiGygU&countryCode=US`;
+    const url = `https://app.ticketmaster.com/discovery/v2/venues.json?keyword=${req.body.searchVenueTerm}&apikey=JsrX5LuYMMOMbdo51ofnq8ud8gHiGygU&countryCode=US`;
     const { data } = await axios.get(url);
+    if (!data._embedded) {
+        res.render("venueNotFound", { searchTerm: req.body.searchVenueTerm });
+        return;
+    }
+
     var listOfVenues = [];
+    let count = 0;
     for (let i = 0; i < data._embedded.venues.length; i++) {
+        if (count >= 10) break;
         listOfVenues.push({
             name: data._embedded.venues[i].name,
             id: data._embedded.venues[i].id,
         });
+        count += 1;
     }
-    res.render("venueSearchResults", { venue: listOfVenues });
+
+    res.render("venueSearchResults", {
+        venue: listOfVenues,
+        searchTerm: req.body.searchVenueTerm,
+    });
 });
 
 router.route("/venuedetails/:id").get(async (req, res) => {
     const url = `https://app.ticketmaster.com/discovery/v2/venues/${req.params.id}.json?apikey=JsrX5LuYMMOMbdo51ofnq8ud8gHiGygU&countryCode=US`;
     const { data } = await axios.get(url);
-    data.images = data.images[0];
-    console.log(data);
+    // console.log(data);
+    if (!data.images)
+        data.images = { url: "/public/images/No_Image_Available.jpg" };
+    else data.images = data.images[0];
+    // console.log(data.images);
     res.render("venueById", data);
 });
 
